c07f659f34a02bcb88c90afc7b83ff9be94a9a84
MateusSM2000 <matt_petao@hotmail.com>
2025-08-19 15:11:52 -0300

    update

---
+++ src/KeywordsTiposPrimitivos.java
@@ -0,0 +1,23 @@
+class KeywordsTiposPrimitivos {
+    public static void main(String[] args) {
+        int number = 1;
+        var number2 = 2;
+        var string = "string";
+        String string2 = "string2";
+        Long numeroLongo = 10000000000L;
+        var numeroLongo2 = 10000000000L;
+        short numeroPequeno = 10;
+        var numeroPequeno2 = 10;
+        float numeroFloat = 10.0f;
+        var numeroFloat2 = 10.0f;
+        double numeroDouble = 10.0;
+        var numeroDouble2 = 10.0d;
+        char letra = 'a';
+        var string3 = "a";
+        boolean verdadeiro = true;
+        var verdadeiro2 = true;
+        boolean falso = false;
+        var falso2 = false;
+        String nulo = null;
+    }
+}
