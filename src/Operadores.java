5352654a87f629b8da7011bf00768af93bbd8409
MateusSM2000 <matt_petao@hotmail.com>
2025-08-19 22:38:36 -0300

    update

---
+++ src/Operadores.java
@@ -0,0 +1,18 @@
+public class Operadores {
+    public static void main(String[] args) {
+        System.out.print("2 + 2 == 4   ");
+        System.out.println(2 + 2 == 4);
+        System.out.print("2 + 2 != 4   ");
+        System.out.println(2 + 2 != 4);
+        System.out.print("2 + 3 >= 4   ");
+        System.out.println(2 + 3 >= 4);
+        System.out.print("2 + 3 <= 4   ");
+        System.out.println(2 + 3 <= 4);
+        System.out.print("!(2 + 3 >= 4)   "); // msm coisa q not
+        System.out.println(!(2 + 3 >= 4));
+        System.out.print("2 == 4 || 2 == 2   "); // ou
+        System.out.println(2 == 4 || 2 == 2);
+        System.out.print("2 == 4 && 2 == 2   "); // e
+        System.out.println(2 == 4 && 2 == 2);
+    }
+}
