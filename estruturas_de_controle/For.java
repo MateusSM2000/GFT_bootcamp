12f1e65f1216881023c0664b578491633e9b0296
MateusSM2000 <matt_petao@hotmail.com>
2025-08-21 18:20:25 -0300

    update

--- estruturas_de_controle/For.java
+++ estruturas_de_controle/For.java
@@ -1,5 +1,53 @@
+import java.util.Scanner;
+
 public class For {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("var i = 0; i <= 10; i++");
+        for (var i = 0; i <= 10; i++) {
+            System.out.println(i);
+            if (i == 10) {
+                System.out.println();
+            }
+        }
+
+        System.out.println("var i = 0; i <= 10; i+=2");
+        for (var i = 0; i <=10; i+=2) {
+            System.out.println(i);
+            if (i == 10) {
+                System.out.println();
+            }
+        }
+
+        System.out.println("var i = 10; i >= 0; i-=2");
+        for (var i = 10; i >=0; i-=2) {
+            System.out.println(i);
+            if (i == 0) {
+                System.out.println();
+            }
+        }
+
+        System.out.println("var i = 10; i >=0; i--  if (i % 2 == 0) { continue; }");
+        for (var i = 10; i >=0; i--) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println(i);
+            if (i == 1) {
+                System.out.println();
+            }
+        }
+
+        float total = 0;
+        for (;;) {
+            System.out.print("Digite um número para somar [0 para sair]: ");
+            float numero = scanner.nextFloat();
+            total += numero;
+            System.out.printf("Total: %s\n", total);
+            if (numero == 0) {
+                break;
+            }
+        }
     }
 }
